638f5035a896d64c630cea37c0b17744a5be9f7f
Navabharath Technologies <dev@navabharathtechnologies.com>
2026-03-05 19:55:23 +0530

    Updated package.json and vite.config.js to clean React setup

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Nidhi-Fresh-Basket-Website/',
-
   plugins: [react()],
-
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-    exclude: [],
-  },
-
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-
-  server: {
-    port: 5173,
-  },
 })
