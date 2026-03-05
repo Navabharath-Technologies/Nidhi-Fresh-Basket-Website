918248b6532cbeae488d066788bc79f86f587b21
Navabharath Technologies <dev@navabharathtechnologies.com>
2026-03-05 19:46:47 +0530

    Update vite config with JSX support for JS files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Nidhi-Fresh-Basket-Website/',
-  plugins: [react()]
+
+  plugins: [react()],
+
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+
+  server: {
+    port: 5173,
+  },
 })
